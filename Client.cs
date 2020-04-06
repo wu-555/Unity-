b3dbbab6cb7a14988cb9099d1c27b36174806736
WoWu <943012535@qq.com>
2020-04-06 23:10:55 +0800

    构建聊天服务器C#代码

---
+++ Client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace 聊天室服务器
+{
+    class Client
+    {
+        //返回Client的连接状态
+        public bool ClientConnected {
+            get {
+                return socket.Connected;
+            }
+        }
+
+        private Socket socket;
+        private byte[] reciveData = new byte[1024];
+        private Thread t;
+        public Client(Socket s)
+        {
+            this.socket = s;
+            t = new Thread(ReciveMessage);//创建线程接收信息
+            t.Start();
+        }
+
+        //接收信息
+        public void ReciveMessage()
+        {
+            while (true)//建立死循环不断地接收消息
+            {
+                if (socket.Poll(10, SelectMode.SelectRead))
+                {
+                    socket.Close();
+                    break;
+                }
+                int length = socket.Receive(reciveData);
+                string reciveMessage = Encoding.UTF8.GetString(reciveData, 0, length);
+                Program.BoradCastMessage(reciveMessage);
+                Console.WriteLine(reciveMessage);
+            }
+        }
+
+        public void SendMessage(string message)
+        {
+            socket.Send(Encoding.UTF8.GetBytes(message));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace 聊天室服务器
+{
+    class Program
+    {
+        public static List<Client> clientList = new List<Client>();
+
+        public static void BoradCastMessage(string message)
+        {
+            var notConnClient = new List<Client>();
+            foreach (var client in clientList)
+            {
+                if (client.ClientConnected)
+                {
+                    client.SendMessage(message);
+                }
+                else
+                {
+                    notConnClient.Add(client);
+                }
+            }
+            foreach (var temp in notConnClient)
+            {
+                clientList.Remove(temp);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Socket tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            tcpSocket.Bind(new IPEndPoint(IPAddress.Parse("192.168.0.102"),7788));
+            tcpSocket.Listen(100);
+            while (true)
+            {
+                Socket socket = tcpSocket.Accept();
+                Client client = new Client(socket);
+                clientList.Add(client);
+            }
+        }
+    }
+}
